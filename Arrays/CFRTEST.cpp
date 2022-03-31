ecfd6ab930ebab5e5dc27af997985662b674b5be
Nishad-007 <mechanics77paradoxes@gmail.com>
2022-03-31 11:39:18 +0530

    solved.

---
+++ Arrays/CFRTEST.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	
+	while(t--){
+	    int n;
+	    cin>>n;
+	    int count=n;
+	    int a[n];
+	    
+	    for(int i=0;i<n;i++){
+	        cin>>a[i];
+	    }
+	    
+	    for(int i=0;i<n;i++){
+	        for(int j=i+1;j<n;j++){
+	            if(a[i]==a[j]){
+	                count--;
+	                break;
+	            }
+	        }
+	    }
+	    cout<<count<<endl;
+	}
+	return 0;
+}
