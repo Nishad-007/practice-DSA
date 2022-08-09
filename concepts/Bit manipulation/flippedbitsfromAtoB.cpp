7d087c62d3a8c244d8ff0c3b092524f938c27bd5
Nishad-007 <mechanics77paradoxes@gmail.com>
2022-08-09 10:41:07 +0530

    flipped bits counted.

---
+++ concepts/Bit manipulation/flippedbitsfromAtoB.cpp
@@ -0,0 +1,29 @@
+//Count number of bits to be flipped to convert A to B
+
+#include <iostream>
+using namespace std;
+
+void flippedbits(int &a, int&b){
+
+    int XOR = a^b;
+    int count =0;
+
+    while (XOR)
+    {   
+        int bit =XOR&1;
+        if(bit){
+            count++;
+        }
+        XOR=XOR>>1;
+    }
+
+    cout<<count<<endl;
+    
+}
+
+int main(){
+    int a;int b;
+    cin>>a>>b;
+
+    flippedbits(a,b);
+}
