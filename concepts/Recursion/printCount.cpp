8dc02bd10a3df78175731ba1bb5ab81d0e468fca
Nishad-007 <mechanics77paradoxes@gmail.com>
2022-08-15 00:12:32 +0530

    completed.

---
+++ concepts/Recursion/printCount.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+void count(int n ){
+    if( n==0 )return ;
+
+    // tail recursion
+    // cout<<n<<endl;
+
+    count(n-1);
+
+    //head recursion
+    cout<<n<<endl;
+}
+
+int main(){
+    int n ;
+    cin >> n ;
+
+    count(n);
+
+
+    return 0;
+}
